3dbf1fe0ef2d2a0e267091c3aaf7c8ce87f848d5
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-05-07 01:08:36 +0100

    feat: add calculation for best path

--- src/test/scala/PathSpec.scala
+++ src/test/scala/PathSpec.scala
@@ -41,7 +41,7 @@ class PathSpec extends AnyFlatSpec with Matchers {
     paths.shortest should be(Some(path1))
   }
 
-  it should "return all paths that include the given stop" in {
+  it should "retrieve all paths that include the given stop" in {
     val path1 =
       Path(segments = List(Segment(Point(0, 0), Point(0, 1)), Segment(Point(0, 1), Point(1, 1))))
     val path2 =
@@ -59,7 +59,7 @@ class PathSpec extends AnyFlatSpec with Matchers {
     paths.includeStopAt(Point(1, 1)) should be(List(path1, path3))
   }
 
-  it should "return all paths that include stops in given points" in {
+  it should "retrieve all paths that include stops in given points" in {
     val path1 =
       Path(segments = List(Segment(Point(0, 0), Point(0, 1)), Segment(Point(0, 1), Point(1, 1))))
     val path2 =
@@ -75,5 +75,23 @@ class PathSpec extends AnyFlatSpec with Matchers {
     val paths = List(path1, path2, path3)
 
     paths.includeStopsIn(List(Point(0, 0), Point(3, 4))) should be(List(path2))
+  }
+
+  it should "calculate the best path that include stops in given points" in {
+    val path1 =
+      Path(segments = List(Segment(Point(0, 0), Point(0, 1)), Segment(Point(0, 1), Point(1, 1))))
+    val path2 =
+      Path(segments = List(Segment(Point(0, 0), Point(3, 4)), Segment(Point(3, 4), Point(3, 3))))
+    val path3 = Path(segments =
+      List(
+        Segment(Point(0, 0), Point(1, 0)),
+        Segment(Point(1, 0), Point(1, 1)),
+        Segment(Point(1, 1), Point(0, 1))
+      )
+    )
+
+    val paths = List(path1, path2, path3)
+
+    paths.best(List(Point(0, 0), Point(1, 1))) should be(Some(path1))
   }
 }
